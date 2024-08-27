527795c7569c7e0a7456c6a96d5368718808b005
Arron Jablonowski <77128909+ArronJablonowski@users.noreply.github.com>
2024-08-27 11:06:48 -0500

    Add files via upload

--- birtha.sh
+++ birtha.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-# ABOUT:
-#  This Script will perform Remote IR on Unix/Ubuntu based Hosts using ssh keypair authentication  
-#       Last Updated: 5.24.21
-#       Author: Arron Jablonowski 
-# USAGE:
-#  Modules can be chosen in the "./Modules.conf" file. Simply comment '#' out lines you don't want to run. 
-#  example: $ ./birtha.sh <User@HostName>
-#  example: $ ./birtha.sh <User@IPaddr>
-#  example: $ ./birtha.sh </path/to/hostlist.txt>   (*Host list should contain one 'User@Hostname' per line.)
-#  example: $ ./birtha.sh </path/to/hostlist.txt>  [./Modules/Modules.conf]
-#  example: $ ./birtha.sh localhost ./Modules/Network_Modules.conf
+#
+# description:
+#       This Script will perform Live IR and Threat Hunting on local or remote hosts using ssh keypair authentication.    
+#
+# usage:
+#       Modules can be chosen in the "./Modules.conf" file. Simply comment '#' out lines you don't want to run. 
+#       example: $ ./birtha.sh <root@HostName>
+#       example: $ ./birtha.sh <root@IPaddr>
+#       ** Host lists should contain one 'User@Hostname' per line.)
+#       example: $ ./birtha.sh </path/to/hostlist.txt>   
+#       example: $ ./birtha.sh </path/to/hostlist.txt>  [./birthaConfigs/Modules.conf]
+#       example: $ ./birtha.sh localhost ./birthaConfigs/Network_Modules.conf
+#
+# about: 
+#	    The Birtha project: https://github.com/ArronJablonowski/birtha 
+# 	    Author: Arron Jablonowski  	
+#       Last Updated: 2023.9.18 
+#
+
+###############################################
+############ Advanced SSH Settings ############
+
+### Set SSH Options here. Recommended to only add to what is listed below. 
+SSH_Options=' -q -oStrictHostKeyChecking=no -oPasswordAuthentication=no ' ## BIRTHA'S ORIGINAL SSH SETTINGS ##
+# SSH_Options=' -q -oStrictHostKeyChecking=no -oPasswordAuthentication=no -oPubkeyAcceptedAlgorithms=+ssh-rsa ' ## example: force algo ssh-rsa for older (outdated) OpenWRT/dropbear versions ## 
+
+###############################################
+# @ll w@rr@n7y 15 nu11 & v01d # 
+
+# Get the current time and format it into a proper directory name to hold the script's results 
+#timestamp=$(date|tr ' ' '_' |tr ':' '.') #replace the spaces in date to "_" &  ':' to '.'  ## Old version 
+timestamp=$(date +%Y_%m_%d_%H_%M_%S%z)
 
 # ASCII ART 
 echo_ascii1(){
@@ -59,7 +80,7 @@ echo_config() {
         else
             ##Skip config lines with any " " blank spaces 
             if !(test -z "${line// }"); then  # ${line// } >> '// ' removes spaces prior to checking 
-                echo "RUN_MODULE: $line"                
+                echo " - Module: $line"                
             fi
         fi 
 	done < "$configFile"
@@ -67,7 +88,7 @@ echo_config() {
 }
 
 check_config(){
-    #Check Config file exists  
+    #Check if Config file exists  
     if test -z "$configFile"; then 
         echo "!!! ERROR !!! -- Config File Not Found!"
         exit  
@@ -77,27 +98,27 @@ check_config(){
 ### MAIN SSH Function ### 
 ### This function does all the heavy lifting by running the scripts on remote hosts and reporting the results  
 Run_LiveIR() {   
-    while IFS= read -r line
+    while IFS= read -r script_module
 	do	
-        newline1=$(echo $line | cut -f4 -d'/') #remove path from string
-        newline2=$(echo $newline1 | cut -f1 -d'.') #remove .sh from script name 
-        #if line is not a comment 
-        if grep -q "\#" <<< "$line"; then #if commented "#" then skip 
-            continue #basically do nothing... 
-        else
-            ##Skip config lines with any " " blank spaces 
-            if !(test -z "${line// }"); then  
-                if test -f $(echo ${line} | sed -e 's/^ *//g;s/ *$//g'); then # Remove leading/trailing spaces and test if config file exists 
-                    #make dir if it does not exist
-                    [[ -d "./Results" ]] || mkdir "./Results"
-                    [[ -d "./Results/$timestamp" ]] || mkdir "./Results/$timestamp"
-                    [[ -d "./Results/$timestamp/$newline2" ]] || mkdir "./Results/$timestamp/$newline2"
-                    echo " ~ $ ssh $1 < $newline1"
-                    ssh $1 < "$(echo ${line} | sed -e 's/^ *//g;s/ *$//g')" > "./Results/$timestamp/$newline2/$1__$newline2.txt" -q -o "StrictHostKeyChecking=no" -o "PasswordAuthentication=no" 2>/dev/null &
+        script_fullname=$(echo $script_module | cut -f4 -d'/') # remove path from string
+        script_module_dir=$(echo $script_module | cut -f3 -d'/') # remove path from string
+        script_name=$(echo $script_fullname | cut -f1 -d'.') # remove .sh from script name 
+        # if line is not a comment 
+        if ! grep -q "\#" <<< "$script_module"; then # if not (!) commented "#" line in birtha config file  
+            if !(test -z "${script_module// }"); then   ## Skip config lines with any " " blank spaces 
+                if test -f $(echo ${script_module} | sed -e 's/^ *//g;s/ *$//g'); then # Remove leading/trailing spaces and test if config file exists 
+                    # make dir(s) if it does not exist
+                    [[ -d "./Results" ]] || mkdir "./Results"  # Ensure the './Results' dir exists 
+                    [[ -d "./Results/$timestamp" ]] || mkdir "./Results/$timestamp" # Make a dir with the current timestamp 
+                    [[ -d "./Results/$timestamp/$script_module_dir" ]] || mkdir "./Results/$timestamp/$script_module_dir"
+                    [[ -d "./Results/$timestamp/$script_module_dir/$script_name" ]] || mkdir "./Results/$timestamp/$script_module_dir/$script_name"
+                    echo " ~ $ ssh $1 < $script_fullname"
+                    ssh $SSH_Options $1 < "$(echo ${script_module} | sed -e 's/^ *//g;s/ *$//g')" > "./Results/$timestamp/$script_module_dir/$script_name/$1__$script_name.txt" 2>/dev/null &
+                    sleep 0.4 # Slow things down a bit. Don't DoS yourself with too many ssh connections at once. 
                 else
-                    echo "!!! ERROR !!! $1 < $line"
+                    echo " X !3rr0r! -- Script NOT Found: $script_module"
                 fi 
-            fi
+            fi            
         fi    
 	done < "$configFile"
     wait # wait for each host to complete before proceeding. 
@@ -106,7 +127,7 @@ Run_LiveIR() {
 #
 #Run_Analysis() { # examp. cat ./Results/Fri_26_Feb_2021_11.03.32_PM_CST/cat_passwd/*
 #    #while IFS= read -r line
-#        cat "./Results/$timestamp/$line/"
+#        cat "./Results/$timestamp/$script_module/"
 #    #done < "$configFile"
 #}
 
@@ -119,19 +140,29 @@ if test -z "$1"; then # $1 is a positional parameter
 	echo "$ ./birtha.sh <user@hostname>"
 	echo "$ ./birtha.sh </path/to/hostlist.txt> (*Host list should contain one user@hostname per line.)"
     exit
+else 
+    reverseit=$(echo $1 | rev) # reverse the order of the string to get the extension first 
+    inputs_extension=$(echo $reverseit | cut -d'.' -f1 | rev) # cut on the '.' and reverse the extension back 
+    echo $inputs_extension
+    if [ "$inputs_extension" == "txt" ]; then 
+        echo "possition 1 is a host (txt) file"
+        sleep 5
+    elif [ "$inputs_extension" == "conf" ]; then 
+        echo "possition 1 is a config (conf) file"
+        sleep 5
+    fi
+
 fi 
 
 if test -z "$2"; then # IF config file param is null, then run the default config 
     #Config file contains instructions to run scripts on remote hosts. 
-    configFile="./Modules/Default_Modules.conf"
+    configFile="./BirthaConfigs/Default_Modules.conf"
     check_config #Check config file exists function 
 else 
     configFile=$2 # set config file location to user input 
     check_config #Check config file exists function 
 fi
 
-# Get the current time and format it into a directory name to hold the script's results 
-timestamp=$(date|tr ' ' '_' |tr ':' '.') #replace the spaces in date to "_" &  ':' to '.'
  
 # Script starts running here 
 if test -f "$1"; then  # Check If file (hostlist) exists 
@@ -152,6 +183,5 @@ fi
 echo " "
 echo ">> Results can be Found in the 'Results' Folder: "
 echo "------------------------------------------------ "
-echo " --> './birtha/Results/$timestamp/{ *HERE* }'"
-echo " "
+echo " --> './birtha-main/Results/$timestamp/{ *HERE* }'"
 echo " "
